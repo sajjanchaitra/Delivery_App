6ab30cf1d406486d7bebcfca929e5242c6afe7f7
100ravthakurr <114392366+100ravthakurr@users.noreply.github.com>
2026-01-17 21:47:53 +0530

    status updated

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const Store = require("../models/Store");
 const { auth, isVendor } = require("../middleware/auth");
 
