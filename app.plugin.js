8807e2033b07a7b8b9665d3f0e33247c62f64ecb
100ravthakurr <114392366+100ravthakurr@users.noreply.github.com>
2026-01-23 09:53:20 +0530

    done

--- app.plugin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function withCleartextTraffic(config) {
-  config.android = config.android || {};
-  config.android.manifest = config.android.manifest || {};
-  config.android.manifest.application = config.android.manifest.application || {};
-  config.android.manifest.application.$ = config.android.manifest.application.$ || {};
-
-  config.android.manifest.application.$["android:usesCleartextTraffic"] = "true";
-  return config;
-};
